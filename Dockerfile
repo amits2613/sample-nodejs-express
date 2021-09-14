67ad981db826abc7bb6ea3d17d89f1573b72d9b1
IamViditAgarwal <viditvivo@gmail.com>
2021-09-14 13:48:09 +0530

    First Commit / Initialising the repo
    - Add the code of the sample nodejs - express server
    - Created the Dockerfile for building the docker
    - Added the github/ workflow actions

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# File : Dockerfile
+# Dockerfile for building the docker for the sample nodejs application
+# Author : Vidit Agarwal
+#
+# Base image we would like to use for our application.
+FROM node:12.18.1
+
+ENV NODE_ENV=development
+
+WORKDIR /app
+
+COPY ["package.json", "package-lock.json*", "./"]
+
+RUN npm install 
+
+COPY . .
+
+# Skipping the docker CMD instruction as that can be passed dynamically
+
---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+ * Simple Nodejs-Express server
+ * 
+ * Code Contributed by:
+ * 
+ * Team : DevOps
+ */
+ const express = require('express')
+ const app = express()
+ var cors = require('cors')
+
+ // Explicitly setting the port to be use
+ const port = 4488
+ 
+ // Using cors library to enable the HTTP Requests on localhost:4488 from another localhost:xxxx port
+ // By default browser blocks request from different ports even if the domain is same
+ app.use(cors())
+ 
+ app.get('/', (req, res) => {
+   const welcomePage = '<h2> Welcome to Demo Sample App </h2><br><h3>This is app version 1.0.0 </h3>';
+   res.setHeader('Content-type','text/html');
+   res.send(welcomePage);
+ })
+ 
+ 
+ app.listen(port, () => {
+ 
+   // ready for listening to the connections
+ 
+   // Server start notification
+   console.log(`Local Server listening at http://localhost:${port}`)
+ });
